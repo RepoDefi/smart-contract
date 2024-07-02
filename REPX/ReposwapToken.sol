1e2bafd2be413ffe1817a4e1ec04fa93d6df6bcf
Reposwap <104736687+RepoDefi@users.noreply.github.com>
2024-07-02 13:00:55 +0600

    ReposwapToken.sol

---
+++ REPX/ReposwapToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ReposwapToken is ERC20 {
+    constructor() ERC20("Reposwap", "REPX") {
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
+}
